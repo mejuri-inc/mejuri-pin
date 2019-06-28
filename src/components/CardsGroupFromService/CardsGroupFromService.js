8bb6f535e920a82bf921f8c782502c30fb6bc5b8
Patricio Gabriel Maseda <patricio.mase@gmail.com>
2019-06-28 20:25:26 -0300

    Dedupe duplicated products from lists when setting

--- src/components/CardsGroupFromService/CardsGroupFromService.js
+++ src/components/CardsGroupFromService/CardsGroupFromService.js
@@ -6,12 +6,21 @@ const flatProducts = (response) => response.reduce((allProds, subGroup) => {
   return allProds.concat(subGroup.products);
 }, []);
 
+const makeUnique = (arr, comp) => {
+  const unique = arr
+    .map(e => e[comp])
+    .map((e, i, final) => final.indexOf(e) === i && i)
+    .filter(e => arr[e]).map(e => arr[e]);
+
+   return unique;
+}
+
 const CardsGroupFromService = ({ data, dataType, setData }) => {
   useEffect(() => {
     if (!data.length) {
       fetch('http://localhost:3000/categories/' + dataType)
         .then(response => response.json())
-        .then(response => setData(flatProducts(response)));
+        .then(response => setData(makeUnique(flatProducts(response), 'id')));
       }
     }, []);
 
