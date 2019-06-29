e58fb69e0334919f920d9a8a26effdb705c50e10
Patricio Gabriel Maseda <patricio.mase@gmail.com>
2019-06-29 15:42:27 -0300

    Add grid layout

--- src/components/CardsGrid/CardsGrid.js
+++ src/components/CardsGrid/CardsGrid.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Card from '../Card/Card';
 import LikeButton from '../LikeButton/LikeButton';
 
-const CardsGroup = ({ data }) => {
+const StyledSection = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-gap: 1.25rem;;
+  max-width: 1024px;
+  margin: auto;
+  padding: 3rem 1.25rem;
+`;
+
+const CardsGrid = ({ data }) => {
   return (
-    <section>
+    <StyledSection>
       {data.map(product => <Card
         {...product}
         key={product.id}>
           <LikeButton product={product} />
       </Card>)}
-    </section>
+    </StyledSection>
   );
 };
 
-export default CardsGroup;
+export default CardsGrid;
